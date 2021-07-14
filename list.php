2d663f32f68a568d384d72ee339af5e1b06af78c
CamiloEspinosaVera1003 <87442254+CamiloEspinosaVera1003@users.noreply.github.com>
2021-07-14 12:42:16 -0500

    Add files via upload

---
+++ list.php
@@ -0,0 +1,197 @@
+<?php
+session_start();
+require_once "Database/Database.php";
+#print_r($_SESSION);exit;
+if ($_SESSION['login'] == null) {
+    echo "<script>alert('Login incorrecto.');</script>";
+    header("Refresh:0 , url=index.html");
+    exit();
+}
+$login = $_SESSION['login'];
+$sql_fetch_todos = "SELECT * FROM productos ORDER BY id ASC";
+$query = mysqli_query($conn, $sql_fetch_todos);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Lista de Productos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="brm.png">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #1bfdef;
+        }
+        .header {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            right: 0px;
+            height: 50px;
+            padding: 5px 13px 11px 0px;
+            width: 100%;
+            color: white;
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 0px;
+            bottom: 0;
+            background-color: #298dba;
+        }
+        .header p {
+            margin-left: 20px;
+            text-align: left;
+        }
+        .button-logout {
+            position: relative;
+            margin-top: -50px;
+            margin-right: 20px;
+            float: right;
+            text-decoration: none;
+            border: transparent;
+            border-radius: 15px;
+            background-color: #e60000;
+            padding: 10px 10px 10px 10px;
+            color: white;
+            transition: 0.5s;
+        }
+        .button-logout:hover {
+            background-color: #D9ddd4;
+            color: red;
+        }
+        .container {
+            margin: 90px auto;
+            margin-bottom: 50px;
+            border-radius: 30px;
+            text-align: center;
+            background-color: white;
+            width: 40%;
+            padding-bottom: 10px;
+        }
+        table th,
+        tr,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px 0px 10px 0px;
+        }
+        table {
+            width: 100%;
+        }
+        th {
+            color: white;
+            background-color: #298dba;
+        }
+        tr {
+            background-color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        .timeregis {
+            text-align: center;
+        }
+        .modify {
+            text-align: center;
+        }
+        .delete {
+            text-align: center;
+        }
+        .modify .bfix {
+            border-radius: 15px;
+            background-color: #ffcc33;
+            color: black;
+            text-decoration: none;
+            padding: 4px 20px 4px 20px;
+            transition: 0.5s;
+        }
+        .modify .bfix:hover {
+            background-color: #fdb515;
+            color: white;
+        }
+        .delete .bdelete {
+            border-radius: 15px;
+            background-color: #e60000;
+            text-decoration: none;
+            color: white;
+            padding: 4px 20px 4px 20px;
+            transition: 0.5s;
+        }
+        .delete .bdelete:hover {
+            background-color: #D9ddd4;
+            color: red;
+        }
+        .Addlist {
+            margin-right: 100px;
+            padding: 5px 30px 5px 30px;
+            border-radius: 15px;
+            text-decoration: none;
+            color: white;
+            background-color: #00A600;
+            transition: 0.5s;
+        }
+        .Addlist:hover {
+            color: black;
+            background-color: #BBFFBB;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h3>Mi inventario</h3>
+        <a name="" id="" class="button-logout" href="logout.php" role="button">Cerrar Sesión</a>
+    </div>
+    <div class="container">
+        <h1>Lista de Productos</h1>
+        <h2>Has accedido como <?php echo $str = strtoupper($login) ?></h2>
+    </div>
+    <div class="table-product">
+        <table>
+            <tr>
+                
+                <th scope="col">ID:Producto</th>
+                <th scope="col">Nombre:Producto</th>
+                <th scope="col">Lote </th>
+                <th scope="col">Valor producto</th>
+                <th scope="col">Cantidades</th>
+                <th scope="col">Fecha Registo</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Eliminar</th>
+            </tr>
+            <tbody>
+                <?php
+                $idpro = 1;
+                while ($row = mysqli_fetch_array($query)) { ?>
+                    <tr>
+                        
+                        <td><?php echo $row['id'] ?></td>
+                        <td><?php echo $row['producto'] ?></td>
+                        <td><?php echo $row['lote'] ?></td>
+                        <td><?php echo $row['precio'] ?></td>
+                        <td><?php echo $row['cantidad_disponible'] ?></td>
+                        <td class="timeregis"><?php echo $row['fecha_vencimiento'] ?></td>
+                        <td class="modify"><a name="edit" id="" class="bfix" href="fix.php?id=<?php echo $row['id'] ?>&message=<?php echo $row['producto'] ?>&amount=<?php echo $row['precio']; ?>&cantidad_disponible=<?php echo $row['cantidad_disponible']; ?>&lote=<?php echo $row['lote']; ?>"  role="button">
+                                Editar
+                            </a></td>
+                        <td class="delete"><a name="id" id="" class="bdelete" href="main/delete.php?id=<?php echo $row['id'] ?>" role="button">
+                                Eliminar
+                            </a></td>
+                    </tr>
+                <?php
+                    $idpro++;
+                } ?>
+            </tbody>
+        </table>
+        <br>
+        <a name="" id="" class="Addlist" style="float:right" href="addlist.php" role="button">Agregar Producto</a>
+        <a name="" id="" class="Addlist" style="float:right" href="vender_producto.php" role="button">Vender Producto</a>
+    </div>
+    <?php
+    mysqli_close($conn);
+    ?>
+</body>
+
+</html>
